a49d710e52f262f3052ac9ae57e2af492b03a3c7
Min-Se <132890400+Min-Se@users.noreply.github.com>
2024-01-06 21:38:38 +0100

    a

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //index.js
-import { getVideo } from "api.js";
+log("JS loaded")
+import { getVideo } from "../api.js";
 document.getElementById("parse-button").addEventListener("click", parseText);
 document.getElementById("clear-button").addEventListener("click", clearInput);
 
